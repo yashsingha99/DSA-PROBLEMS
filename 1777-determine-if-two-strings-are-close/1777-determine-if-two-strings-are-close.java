21fbea27dc593a268bf7b62782b2b48267f24be5
Yash Singhal <122280082+yashsingha99@users.noreply.github.com>
2024-08-22 16:05:33 +0530

    Time: 194 ms (5.15%), Space: 45.6 MB (36.99%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
@@ -4,6 +4,7 @@ class Solution {
         if(n1 != n2) return false;
         int arr1[] = new int[26];
         int arr2[] = new int[26];
+        int dig[] = new int[100000];
         for(char c : word1.toCharArray()){
             if(word2.indexOf(c) == -1) return false;
             arr1[c - 'a']++;
@@ -12,12 +13,16 @@ class Solution {
             if(word1.indexOf(c) == -1) return false;
             arr2[c - 'a']++;
         }
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
         for(int i = 25; i >= 0; i--){
-            if(arr1[i] == 0 && arr2[i] == 0) break;
-            if(arr1[i] != arr2[i]) return false;
+            dig[arr1[i]]++;
+            dig[arr2[i]]--;
         }
+        for(int i = 0; i < dig.length; i++)
+           if(dig[i] != 0) return false;
+        // for(int i = 25; i >= 0; i--){
+        //     if(arr1[i] == 0 && arr2[i] == 0) break;
+        //     if(arr1[i] != arr2[i]) return false;
+        // }
         return true;
     }
 }
