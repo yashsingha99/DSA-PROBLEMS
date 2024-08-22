7496e990e808a38415e6558783922541bbd2502f
Yash Singhal <122280082+yashsingha99@users.noreply.github.com>
2024-08-22 16:35:57 +0530

    Time: 358 ms (5.15%), Space: 45.8 MB (25.52%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
@@ -4,25 +4,24 @@ class Solution {
         if(n1 != n2) return false;
         int arr1[] = new int[26];
         int arr2[] = new int[26];
-        int dig[] = new int[100000];
-        for(char c : word1.toCharArray()){
-            if(word2.indexOf(c) == -1) return false;
-            arr1[c - 'a']++;
+        for(int i = 0; i < n1; i++){
+            char ch1 = word1.charAt(i);
+            char ch2 = word2.charAt(i);
+            if(word2.indexOf(ch1) == -1 || word1.indexOf(ch2) == -1) return false;
+            arr1[ch1 - 'a']++;
+            arr2[ch2 - 'a']++;
         }
-        for(char c : word2.toCharArray()){
-            if(word1.indexOf(c) == -1) return false;
-            arr2[c - 'a']++;
+        for (int i = 0; i < 26; i++) {
+            if ((arr1[i] == 0 && arr2[i] != 0) || (arr1[i] != 0 && arr2[i] == 0)) {
+                return false;
+            }
         }
-        for(int i = 25; i >= 0; i--){
-            dig[arr1[i]]++;
-            dig[arr2[i]]--;
-        }
-        for(int i = 0; i < dig.length; i++)
-           if(dig[i] != 0) return false;
-        // for(int i = 25; i >= 0; i--){
-        //     if(arr1[i] == 0 && arr2[i] == 0) break;
-        //     if(arr1[i] != arr2[i]) return false;
-        // }
+        
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+         for (int i = 0; i < 26; i++)
+            if (arr1[i] != arr2[i])
+                return false;
         return true;
     }
 }
