0ea0a33f14e0b08cf087512505941334c73bcb30
Yash Singhal <122280082+yashsingha99@users.noreply.github.com>
2024-08-24 19:46:46 +0530

    Time: 60 ms (21.93%), Space: 55.7 MB (65.24%) - LeetHub

---
+++ 1375-find-palindrome-with-fixed-length/1375-find-palindrome-with-fixed-length.java
@@ -0,0 +1,30 @@
+class Solution {
+    String nthPalindrom(int n, int k) {
+        int temp = (k & 1) != 0 ? k / 2 : (k / 2) - 1;
+        int pal = (int)Math.pow(10, temp);
+        pal += n - 1;
+        StringBuilder sb = new StringBuilder(Integer.toString(pal));
+        if ((k & 1) > 0)
+            pal /= 10;
+
+        while (pal > 0) {
+            sb.append(pal % 10);
+            pal /= 10;
+        }
+        return sb.toString();
+    }
+
+    public long[] kthPalindrome(int[] q, int len) {
+        int n = q.length;
+        long[] res = new long[n];
+        for (int i = 0; i < n; i++) {
+            String num = nthPalindrom(q[i], len);
+            if (num.length() == len)
+                res[i] = Long.parseLong(num);
+            else
+                res[i] = -1;
+        }
+        return res;
+
+    }
+}
