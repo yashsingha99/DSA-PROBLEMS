f2f0f50eca3fc92d7c36f6c38a3356032a61960f
Yash Singhal <122280082+yashsingha99@users.noreply.github.com>
2024-09-11 20:02:11 +0530

    Time: 2 ms (93.74%), Space: 42.6 MB (21.8%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -3,12 +3,11 @@ class Solution {
         Stack<Character> st = new Stack<>();
         int[] arr1 = new int[26], arr2 = new int[26];
         int n = s.length();
-        for (int i = n - 1; i >= 0; i--)
+        for (int i = n - 1; i >= 0; i--){
             if (arr2[s.charAt(i) - 'a'] == 0)
                 arr2[s.charAt(i) - 'a'] = i;
-        st.push(s.charAt(0));
-        arr1[s.charAt(0) - 'a'] = 1;
-        for (int i = 1; i < n; i++) {
+        }
+        for (int i = 0; i < n; i++) {
             if (arr1[s.charAt(i) - 'a'] == 1) continue;
             while (!st.empty() && st.peek() > s.charAt(i) && arr2[st.peek() - 'a'] > i) {
                 arr1[st.pop() - 'a'] = 0;
