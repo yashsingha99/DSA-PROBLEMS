e29e2c37e64b2b58ca0c34914ecae3e0e4a14342
Yash Singhal <122280082+yashsingha99@users.noreply.github.com>
2024-09-20 19:30:39 +0530

    Time: 57 ms (43.07%), Space: 47.2 MB (57.03%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
@@ -1,18 +1,26 @@
 class Solution {
     public String sortVowels(String s) {
         char[] c = s.toCharArray();
-        String vowels = "aeiouAEIOU";
+        String vowels = "AEIOUaeiou";
         int n = c.length;
-        List<Character> list = new ArrayList<>();
-        for(int i = 0;  i < n; i++){
-           if(vowels.indexOf(c[i]) != -1)
-               list.add(c[i]);
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : c) {
+            if (vowels.indexOf(ch) != -1)
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-        Collections.sort(list);
-        int idx = 0;
-        for(int i = 0; i < n; i++){
-            if(vowels.indexOf(c[i]) != -1)
-               c[i] = list.get(idx++);
+        for (int i = 0; i < n; i++) {
+            if (vowels.indexOf(c[i]) == -1)
+                continue;
+            for (char ch : vowels.toCharArray()) {
+                if (map.containsKey(ch)) {
+                    c[i] = ch;
+                    map.put(ch, map.get(ch) - 1);
+                    if (map.get(ch) == 0)
+                        map.remove(ch);
+                    break;
+                }
+            }
+
         }
         return new String(c);
     }
