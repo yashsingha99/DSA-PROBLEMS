e152d563b2f49df370767f9ce9f10699299a913a
Yash Singhal <122280082+yashsingha99@users.noreply.github.com>
2024-09-23 20:13:26 +0530

    Time: 45 ms (55.1%), Space: 45.5 MB (13.27%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.java
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.java
@@ -1,25 +1,17 @@
 class Solution {
-    int n = 0;
-    Set<String> set;
-    int[] dp;
-    int solve(String s, int idx){
-        if(idx >= n) return 0;
-        if(dp[idx] != -1) return dp[idx];
-        int res = 1  + solve(s, idx + 1);
-        for(int i = idx; i < n; i++){
-            if(set.contains(s.substring(idx, i + 1)))
-              res = Math.min(res, solve(s, i + 1));
-        }
-        return dp[idx] = res;
-    }
     public int minExtraChar(String s, String[] dic) {
-       n = s.length();
-       set = new HashSet<>();
-       dp = new int[n + 1];
-       Arrays.fill(dp, -1);
-       for(String str : dic)
+        int n = s.length();
+        Set<String> set = new HashSet<>();
+        for(String str : dic)
           set.add(str);
-       
-       return solve(s, 0);
+        int[] dp = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = 1 + dp[i + 1];
+            for (int j = i; j < n; j++) {
+                if (set.contains(s.substring(i, j + 1)))
+                    dp[i] = Math.min(dp[i], dp[j + 1]);
+            }
+        }
+        return dp[0];
     }
 }
