f1a3863533d7aeaaf2c82869d65bf2b0be54b4ad
Yash Singhal <122280082+yashsingha99@users.noreply.github.com>
2024-09-24 17:09:57 +0530

    Time: 34 ms (91.99%), Space: 56.3 MB (35.34%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.java
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.java
@@ -1,21 +1,43 @@
+class TrieNode {
+    TrieNode[] arr = new TrieNode[10];
+    char dig;
+}
+
 class Solution {
-    public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        HashSet<Integer> set = new HashSet<>();
-        int ans = 0;
-        for(int num : arr1){
-            while(num != 0){
-                if(!set.contains(num))
-                  set.add(num);
-                num /= 10;
-            }
+    void insert(int num, TrieNode root) {
+        TrieNode temp = root;
+        String str = Integer.toString(num);
+        for (char ch : str.toCharArray()) {
+            int idx = ch - '0';
+            if (temp.arr[idx] == null)
+                temp.arr[idx] = new TrieNode();
+            temp = temp.arr[idx];
         }
-        for(int num : arr2){
-            while(num != 0){
-                if(set.contains(num)){
-                  ans = Math.max(ans, (int)Math.log10(num) + 1);
-                }
-                num /= 10;
-            }
+    }
+
+    int search(int num, TrieNode root) {
+        TrieNode temp = root;
+        String str = Integer.toString(num);
+        int cnt = 0;
+        for (char ch : str.toCharArray()) {
+            int idx = ch - '0';
+            if (temp.arr[idx] != null) {
+                cnt++;
+                temp = temp.arr[idx];
+            } else
+                break;
+        }
+        return cnt;
+    }
+
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        int ans = 0;
+        TrieNode root = new TrieNode();
+        for (int num : arr1) {
+            insert(num, root);
+        }
+        for (int num : arr2) {
+            ans = Math.max(ans, search(num, root));
         }
         return ans;
     }
