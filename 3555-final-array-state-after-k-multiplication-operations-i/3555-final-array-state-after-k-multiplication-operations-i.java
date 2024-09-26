6f67dd9294918a769a041d5f140f2e5db715b896
Yash Singhal <122280082+yashsingha99@users.noreply.github.com>
2024-09-26 14:23:00 +0530

    Time: 4 ms (14.54%), Space: 45.2 MB (7.52%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
@@ -1,19 +1,24 @@
 class Solution {
-    public int[] getFinalState(int[] nums, int k, int mul) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            if(a[0] == b[0])
-              return a[1] - b[1];
-            else 
-              return a[0] - b[0];
-        });
-        for(int i = 0; i < nums.length; i++){
-            pq.add(new int[]{nums[i], i});
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+                (a, b) -> {
+                    if (a[0] != b[0])
+                        return Integer.compare(a[0], b[0]);
+                    return Integer.compare(a[1], b[1]);
+                });
+
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            pq.add(new int[] { nums[i], i });
         }
-        while(k-- > 0){
-           int[] arr = pq.poll();
-           arr[0] *= mul;
-           pq.add(arr);
-           nums[arr[1]] *= mul;
+        while (k-- > 0) {
+            int[] A = pq.poll();
+            A[0] *= multiplier;
+            pq.add(A);
+        }
+        while (!pq.isEmpty()) {
+            int[] A = pq.poll();
+            nums[A[1]] = A[0]; 
         }
         return nums;
     }
