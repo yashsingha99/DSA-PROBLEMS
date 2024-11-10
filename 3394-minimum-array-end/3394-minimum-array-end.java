722f3bd315ae562ab99f18fabe319c483b46b21b
Yash Singhal <122280082+yashsingha99@users.noreply.github.com>
2024-11-10 21:43:15 +0530

    Time: 1 ms (87.72%), Space: 40.9 MB (51.46%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.java
+++ 3394-minimum-array-end/3394-minimum-array-end.java
@@ -1,8 +1,19 @@
-class Solution {
+ class Solution {
     public long minEnd(int n, int x) {
-        long ans = x;
-        while(n-- > 1){
-           ans = (x | (ans + 1));
+        int bit[] = new int[64];
+        n--; // to find direct n - 1 value
+        for (int i = 0; i < 32; i++) {
+            bit[i] = (x >> i) & 1;
+        }
+        for (int i = 0; i < 64 && n != 0; i++) {
+            if (bit[i] == 0) {
+                bit[i] = (n & 1);
+                n >>= 1;
+            }
+        }
+        long ans = 0;
+        for(int i = 0; i < 64; i++){
+             ans += bit[i] * (1l << i);
         }
         return ans;
     }
