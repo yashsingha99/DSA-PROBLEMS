5e6afa3bc088ea07f3814d735ed0fb3f2f6de921
Yash Singhal <122280082+yashsingha99@users.noreply.github.com>
2025-01-21 16:21:21 +0530

    Time: 4 ms (97.41%), Space: 57.3 MB (84.46%) - LeetHub

--- 2145-grid-game/2145-grid-game.java
+++ 2145-grid-game/2145-grid-game.java
@@ -1,21 +1,17 @@
 class Solution {
     public long gridGame(int[][] grid) {
         int n = grid[0].length;
-        long[][] mat = new long[2][n];
-        mat[0][0] = grid[0][0];
-        mat[1][n - 1] = grid[1][n - 1]; 
-        for (int i = 1; i < n; i++)
-            mat[0][i] = mat[0][i - 1] + grid[0][i];
+        long suf = 0, pre = 0;
 
-        for (int i = n - 2; i >= 0; i--)
-            mat[1][i] = mat[1][i + 1] + grid[1][i];
+        for (int i : grid[0])
+            pre += i;
 
         long min = Long.MAX_VALUE;
 
         for (int i = 0; i < n; i++) {
-            long row1 = mat[0][n - 1] - mat[0][i];
-            long row2 = mat[1][0] - mat[1][i];
-            min = Math.min(min, Math.max(row1, row2));
+             pre  -= grid[0][i];
+             min = Math.min(min, Math.max(suf, pre));
+             suf  += grid[1][i];
         }
         return min;
     }
