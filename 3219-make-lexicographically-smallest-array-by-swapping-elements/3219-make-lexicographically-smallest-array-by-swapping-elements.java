2c0a72da605e2381dec5040559ff6c84bac18c82
Yash Singhal <122280082+yashsingha99@users.noreply.github.com>
2025-01-25 23:58:33 +0530

    Time: 84 ms (68.69%), Space: 72.4 MB (28.28%) - LeetHub

---
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+        int n = nums.length;
+        int[] vec = nums.clone();
+        Arrays.sort(vec);
+
+        int groupNum = 0;
+        Map<Integer, Integer> numToGroup = new HashMap<>();
+        Map<Integer, LinkedList<Integer>> groupToList = new HashMap<>();
+
+        numToGroup.put(vec[0], groupNum);
+        groupToList.putIfAbsent(groupNum, new LinkedList<>());
+        groupToList.get(groupNum).add(vec[0]);
+
+        for (int i = 1; i < n; i++) {
+            if (Math.abs(vec[i] - vec[i - 1]) > limit) {
+                groupNum++;
+            }
+            numToGroup.put(vec[i], groupNum);
+            groupToList.putIfAbsent(groupNum, new LinkedList<>());
+            groupToList.get(groupNum).add(vec[i]);
+        }
+
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            int group = numToGroup.get(num);
+            result[i] = groupToList.get(group).pollFirst(); 
+        }
+
+        return result;
+    }
+}
