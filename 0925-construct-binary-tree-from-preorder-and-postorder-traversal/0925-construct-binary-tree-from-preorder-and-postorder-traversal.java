7e36eaa49fa023f5c6b4352266b5a4cfac15e1a7
Yash Singhal <122280082+yashsingha99@users.noreply.github.com>
2025-02-23 10:38:08 +0530

    Time: 0 ms (100%), Space: 43.7 MB (32.59%) - LeetHub

--- 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.java
+++ 0925-construct-binary-tree-from-preorder-and-postorder-traversal/0925-construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -1,24 +1,21 @@
 class Solution {
     int i, j;
-    Set<Integer> set = new HashSet<>();
-    TreeNode construct(int[] pre, int[] post){
-        if(i >= pre.length) return null;
-         if(set.contains(post[j]))   return null;
-        if(pre[i] == post[j]){
-              j++;
-            return new TreeNode(pre[i++]);
-        }
-        TreeNode root = new TreeNode(pre[i]);
-        set.add(pre[i]);
-        i++;
-        root.left = construct(pre, post);
-        // if(set.contains(post[j]))  
-        root.right = construct(pre, post);
+
+    TreeNode construct(int[] pre, int[] post) {
+        if (i >= pre.length)
+            return null;
+        TreeNode root = new TreeNode(pre[i++]);
+        if (root.val != post[j])
+            root.left = construct(pre, post);
+        if (root.val != post[j])
+            root.right = construct(pre, post);
         j++;
         return root;
     }
+
     public TreeNode constructFromPrePost(int[] pre, int[] post) {
-        i = 0; j = 0;
+        i = 0;
+        j = 0;
         return construct(pre, post);
     }
 }
