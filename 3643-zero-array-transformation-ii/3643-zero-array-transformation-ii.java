83e1733b430a7fd03bfa866a0a5408940a325893
Yash Singhal <122280082+yashsingha99@users.noreply.github.com>
2025-03-13 16:07:20 +0530

    Time: 17 ms (66.29%), Space: 108.1 MB (83.29%) - LeetHub

---
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.java
@@ -0,0 +1,44 @@
+class Solution {
+
+    public int minZeroArray(int[] nums, int[][] queries) {
+        int n = nums.length, left = 0, right = queries.length;
+
+        // Zero array isn't formed after all queries are processed
+        if (!currentIndexZero(nums, queries, right)) return -1;
+
+        // Binary Search
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (currentIndexZero(nums, queries, middle)) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        // Return earliest query that zero array can be formed
+        return left;
+    }
+
+    private boolean currentIndexZero(int[] nums, int[][] queries, int k) {
+        int n = nums.length, sum = 0;
+        int[] differenceArray = new int[n + 1];
+
+        // Process query
+        for (int queryIndex = 0; queryIndex < k; queryIndex++) {
+            int left = queries[queryIndex][0], right =
+                queries[queryIndex][1], val = queries[queryIndex][2];
+
+            // Process start and end of range
+            differenceArray[left] += val;
+            differenceArray[right + 1] -= val;
+        }
+
+        // Check if zero array can be formed
+        for (int numIndex = 0; numIndex < n; numIndex++) {
+            sum += differenceArray[numIndex];
+            if (sum < nums[numIndex]) return false;
+        }
+        return true;
+    }
+}
