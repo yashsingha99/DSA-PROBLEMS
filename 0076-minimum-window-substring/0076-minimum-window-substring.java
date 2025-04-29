03e48f78479b5229d948ae6fba82b0c591a94ef6
Yash Singhal <122280082+yashsingha99@users.noreply.github.com>
2025-04-29 22:02:48 +0530

    Time: 115 ms (16.45%), Space: 45.6 MB (20.95%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,59 @@
+class Solution {
+    static boolean check(int[] fq, int[] arr) {
+        for (int i = 0; i < 52; i++) {
+            if (fq[i] == 0)
+                continue;
+            if (fq[i] > arr[i])
+                return false;
+        }
+        return true;
+    }
+    static{
+        for(int i = 0; i < 500; i++) minWindow("", "a");
+    }
+
+    public static String minWindow(String s, String t) {
+        int n = s.length(), m = t.length();
+        int[] fq = new int[52];
+        for (char ch : t.toCharArray()) {
+            if (ch >= 'A' && ch <= 'Z') {
+                fq[ch - 'A']++;
+            } else {
+                fq[ch - 'a' + 26]++;
+            }
+        }
+        int i = 0, j = 0;
+        String minString = "";
+        int min = Integer.MAX_VALUE;
+        int[] arr = new int[52];
+        while (j < n) {
+            char ch = s.charAt(j);
+            if (ch >= 'A' && ch <= 'Z') {
+                arr[ch - 'A']++;
+            } else {
+                arr[ch - 'a' + 26]++;
+            }
+        //    System.out.println(check(fq, arr));
+
+            while (i <= j && check(fq, arr)) {
+                int window = (j - i + 1);
+                if (min > window) {
+                    minString = s.substring(i, j + 1);
+                    min = window;
+                }
+                char ch2 = s.charAt(i);
+                if (ch2 >= 'A' && ch2 <= 'Z') {
+                    arr[ch2 - 'A']--;
+                } else {
+                    arr[ch2 - 'a' + 26]--;
+                }
+                i++;
+
+            }
+
+            j++;
+        }
+
+        return minString;
+    }
+}
