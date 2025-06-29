5f60082161a5a1c8d971780e7a6723fd96fc2fc1
Yash Singhal <122280082+yashsingha99@users.noreply.github.com>
2025-06-29 15:05:25 +0530

    Time: 3 ms (99.91%), Space: 55 MB (65.31%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.java
+++ 0917-boats-to-save-people/0917-boats-to-save-people.java
@@ -1,15 +1,22 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        Arrays.sort(people);
-        int n = people.length;
-        int i = 0, j = n - 1, cnt = 0;
-        while(i <= j){
-            if(people[j] + people[i] <= limit){
-                cnt++; i++; j--;
-            } else {
-                cnt++; j--;
-            }
+        int[] arr = new int[limit + 1];
+        for(int wt : people){
+            arr[wt]++;
         }
-        return cnt;
+        int i = 0, j = limit, boats = 0;
+        while(i <= j){
+            if(arr[i] <= 0) i++;
+            else if(arr[j] <= 0) j--;
+            else if(i + j <= limit) {
+                arr[i]--;
+                arr[j]--; 
+                boats++;
+            } else {
+                arr[j]--;
+                boats++;
+            }
+        } 
+        return boats;
     }
 }
