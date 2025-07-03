cdfb4e29e7c99e89e258121f51a4599842809b09
Yash Singhal <122280082+yashsingha99@users.noreply.github.com>
2025-07-03 11:15:10 +0530

    Time: 427 ms (14.84%), Space: 46.8 MB (63.61%) - LeetHub

--- 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/3633-maximize-the-number-of-target-nodes-after-connecting-trees-i.java
+++ 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/3633-maximize-the-number-of-target-nodes-after-connecting-trees-i.java
@@ -1,27 +1,23 @@
 class Solution {
 
-    private int dfs(int curr, Map<Integer, List<Integer>> adj, int d, int parent) {
+    int dfs(int cur, Map<Integer, List<Integer>> adj, int d, int p) {
         if (d < 0)
             return 0;
-
-        int count = 1;
-
-        for (int neighbor : adj.getOrDefault(curr, new ArrayList<>())) {
-            if (neighbor != parent) {
-                count += dfs(neighbor, adj, d - 1, curr);
+        int cnt = 1;
+        for (int neigh : adj.getOrDefault(cur, new ArrayList<>())) {
+            if (neigh != p) {
+                cnt += dfs(neigh, adj, d - 1, cur);
             }
         }
-
-        return count;
+        return cnt;
     }
 
-    private int[] findCount(int[][] edges, int d) {
+    int[] findCount(int[][] edges, int k) {
         int N = edges.length + 1;
         Map<Integer, List<Integer>> adj = new HashMap<>();
-
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
+        for (int A[] : edges) {
+            int u = A[0];
+            int v = A[1];
             if (!adj.containsKey(u))
                 adj.put(u, new ArrayList<>());
             adj.get(u).add(v);
@@ -29,29 +25,25 @@ class Solution {
             if (!adj.containsKey(v))
                 adj.put(v, new ArrayList<>());
             adj.get(v).add(u);
-
         }
 
         int[] res = new int[N];
-        for (int i = 0; i < N; i++) {
-            res[i] = dfs(i, adj, d, -1);
-        }
 
+        for (int i = 0; i < N; i++) {
+            res[i] = dfs(i, adj, k, -1);
+        }
         return res;
     }
 
     public int[] maxTargetNodes(int[][] edges1, int[][] edges2, int k) {
-        int N = edges1.length + 1;
-
         int[] res1 = findCount(edges1, k);
         int[] res2 = findCount(edges2, k - 1);
-
+        int N = edges1.length + 1;
         int max = Arrays.stream(res2).max().getAsInt();
 
         for (int i = 0; i < N; i++) {
             res1[i] += max;
         }
-
         return res1;
     }
 }
